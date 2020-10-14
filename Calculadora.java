9f9990a9d185226893701dde18683f3252a642fc
Miguel <ramirezmiguel@engineer.com>
2020-10-13 19:09:09 -0500

    Mi segundo commit

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Calculadora {
-    
+
     public static void main (String[] args){
+        String nombre;
+        System.out.println("Hola Mundo!");
+        Scanner sc = new Scanner(System.in);
+        nombre = sc.next();
+        System.out.println("Hola"+nombre+"!");
 
     }
 }
